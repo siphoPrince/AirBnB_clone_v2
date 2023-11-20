71fd2353faec364c734d717c6b4e0682cc269e29
Odarell35 <leratomawash0@gmail.com>
2023-11-20 02:57:14 -0800

    updated created at basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -8,19 +8,21 @@ class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        from models import storage
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        storage.new(self)
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for i, j in kwargs.items():
+                if i in ("created_at", "updated_at"):
+                    j = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                if "__class__" not in i:
+                    setattr(self, i, j)
 
     def __str__(self):
         """Returns a string representation of the instance"""
