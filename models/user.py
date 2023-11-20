ec6523728bc5c515363f1372bfebc920a6e0a0e9
siphoPrince <mabirimisesipho@gmail.com>
2023-11-20 11:45:50 -0800

    displaying users

--- models/user.py
+++ models/user.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from os import getenv
+from sqlalchemy import Column, Integer, String
 from models.base_model import BaseModel, Base
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from models.review import Review
-from models.place import Place
+
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
@@ -16,9 +12,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship('Place', backref='user', cascade='delete')
-    reviews = relationship("Review", backref="user", cascade="delete")
-
-    def __init__(self, *args, **kwargs):
-        """ init of User"""
-        super().__init__(*args, **kwargs)
