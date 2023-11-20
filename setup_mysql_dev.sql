f85af7f259bc7319e1be368d4ababbe60cf535b5
siphoPrince <mabirimisesipho@gmail.com>
2023-11-20 01:56:08 -0800

    creating mysql file

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- this file is to prepare for the project using mysql --
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY "hbnb_test_pwd";
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO  hbnb_test@localhost;
+GRANT SELECT ON performance_schema.* TO  hbnb_test@localhost;
