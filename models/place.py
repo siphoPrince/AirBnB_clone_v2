cd53814170a610fbb7f3c9f90aa5ecb86acc6ff4
Odarell35 <leratomawash0@gmail.com>
2023-11-22 12:39:27 -0800

    updating place

--- models/place.py
+++ models/place.py
@@ -2,13 +2,20 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from models.city import City
-from models.user import User
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Table
+from os import getenv
+import models
+from models.review import Review
+from models.amenity import Amenity
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    #user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False)
@@ -17,5 +24,4 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
 
