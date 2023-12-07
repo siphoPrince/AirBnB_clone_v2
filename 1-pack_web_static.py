39e19b58b8e28e277ad850d1b7fc31d6021d661d
siphoPrince <mabirimisesipho@gmail.com>
2023-12-06 23:16:39 -0800

    pack web static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Fabric script to generate tgz archive
+execute: fab -f 1-pack_web_static.py do_pack
+"""
+
+from datetime import datetime
+from fabric.api import local, task
+
+@task
+def do_pack():
+    """
+    Making an archive in the web_static folder
+    """
+    time = datetime.now()
+    archive = f"web_static_{time.strftime('%Y%m%d%H%M%S')}.tgz"
+    local("mkdir -p versions")
+    result = local(f"tar -cvzf versions/{archive} web_static")
+    return archive if result.succeeded else None
+
