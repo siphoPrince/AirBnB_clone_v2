71cf36d2072a76eb80ce15e475d4ad13b282d2f4
siphoprince <mabirimisesipho@gmail.com>
2023-12-08 11:52:46 +0000

    making directory

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,34 @@
 #!/usr/bin/env bash
-# script that sets up web servers for the deployment of web_static
+# setting server for deployment
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 
+# Create directories
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
+
+# Create index.html
+sudo tee /data/web_static/releases/test/index.html > /dev/null <<EOF
+<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>
+EOF
 
+# Create symbolic link
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
+# Set permissions
 sudo chown -R ubuntu:ubuntu /data/
 
+# Configure Nginx
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-
 sudo service nginx restart
+
