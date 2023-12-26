09d8163eaf8de1e274dfcfcff5d90de57cd7db12
siphoPrince <mabirimisesipho@gmail.com>
2023-12-26 07:29:56 -0800

    hello route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
+""" 0. Script to start a Flask web application """
+
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    '''
-    Hello Flask route
-    '''
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Returns some text. """
     return 'Hello HBNB!'
 
-
 if __name__ == '__main__':
-    app.run()
-    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
