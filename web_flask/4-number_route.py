13f862d9d6df677e22be2638d35f68d0e1533c64
siphoPrince <mabirimisesipho@gmail.com>
2023-12-26 07:56:46 -0800

    route n

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,38 +1,37 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+""" adding n"""
+
 from flask import Flask, escape
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Returns 'Hello HBNB!'."""
     return 'Hello HBNB!'
-
 
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
+    """Returns 'HBNB'."""
     return 'HBNB'
-
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_c_text(text):
-    # Replace underscore symbols with a space
+    """Replaces underscore symbols with a space and returns 'C {}'."""
     text = escape(text).replace('_', ' ')
     return 'C {}'.format(text)
-
 
 @app.route('/python/<text>', strict_slashes=False)
 @app.route('/python/', strict_slashes=False)
 def display_python_text(text='is cool'):
-    # Replace underscore symbols with a space
+    """Replaces underscore symbols with a space and returns 'Python {}'."""
     text = escape(text).replace('_', ' ')
     return 'Python {}'.format(text)
 
-
 @app.route('/number/<int:n>', strict_slashes=False)
 def display_number(n):
+    """Returns '{} is a number'."""
     return '{} is a number'.format(n)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
