b8e31b073ae8704a2c4385ee8f51712865426e2e
siphoPrince <mabirimisesipho@gmail.com>
2023-12-26 08:02:38 -0800

    number templ

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,11 +1,46 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""displaying in format"""
 
 from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Returns 'Hello HBNB!'."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """Returns 'HBNB'."""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    """Replaces underscore symbols with a space and returns 'C {}'."""
+    text = escape(text).replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def display_python_text(text='is cool'):
+    """Replaces underscore symbols with a space and returns 'Python {}'."""
+    text = escape(text).replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def display_number(n):
+    """Returns '{} is a number'."""
+    return '{} is a number'.format(n)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """ return in format"""
     return '{} is a number'.format(n)
 
 
