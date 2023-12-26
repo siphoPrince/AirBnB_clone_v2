c6416c233b5829dc9515a8b95762d679ac22dce6
siphoPrince <mabirimisesipho@gmail.com>
2023-12-26 07:58:59 -0800

    space fix

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -5,21 +5,25 @@ from flask import Flask, escape
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Returns 'Hello HBNB!'."""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     """Returns 'HBNB'."""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def display_c_text(text):
     """Replaces underscore symbols with a space and returns 'C {}'."""
     text = escape(text).replace('_', ' ')
     return 'C {}'.format(text)
+
 
 @app.route('/python/<text>', strict_slashes=False)
 @app.route('/python/', strict_slashes=False)
@@ -28,10 +32,12 @@ def display_python_text(text='is cool'):
     text = escape(text).replace('_', ' ')
     return 'Python {}'.format(text)
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def display_number(n):
     """Returns '{} is a number'."""
     return '{} is a number'.format(n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
