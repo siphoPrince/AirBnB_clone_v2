70b8932e21e9c3bddebb2ac6843fc62a8b4c6219
siphoPrince <mabirimisesipho@gmail.com>
2023-12-26 23:52:04 -0800

    n

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -40,7 +40,7 @@ def display_number(n):
     return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def display_number_template(n):
     """Return in format"""
     return render_template('5-number.html', n=n)
