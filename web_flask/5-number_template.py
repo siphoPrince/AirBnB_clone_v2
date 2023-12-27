1ff54c8636d0a28968f11c4f62e0f964523b82e0
siphoPrince <mabirimisesipho@gmail.com>
2023-12-26 23:38:10 -0800

    render fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,7 +2,7 @@
 """displaying in format"""
 
 from flask import Flask
-from flask import render_template, escape
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -40,7 +40,7 @@ def display_number(n):
     return '{} is a number'.format(n)
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def display_number_template(n):
     """Return in format"""
     return render_template('5-number.html', n=n)
