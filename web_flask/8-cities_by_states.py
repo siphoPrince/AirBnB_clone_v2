4a3aede0844332f2e3824d25fa004c241e76dc00
siphoPrince <mabirimisesipho@gmail.com>
2023-12-29 23:08:14 -0800

    pycode

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-Starts a Flask web application to display states and cities in alphabetical order.
+Starts a Flask web application to display
+states and cities in alphabetical order.
 """
+
 
 from flask import Flask, render_template
 from models import *
@@ -10,16 +12,20 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Display a web page with a list of states and their cities in alphabetical order."""
+    """Display a web page with a list of
+    states and their cities in alphabetical order."""
     states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """Close the storage connection on application teardown."""
     storage.close()
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000', debug=True)
