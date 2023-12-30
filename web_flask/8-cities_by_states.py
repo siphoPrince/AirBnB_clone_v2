428ac2fc4c480108e0ccd1702942aae79cca4289
siphoPrince <mabirimisesipho@gmail.com>
2023-12-29 23:29:03 -0800

    state order

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -18,6 +18,7 @@ def cities_by_states():
     """Display a web page with a list of
     states and their cities in alphabetical order."""
     states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
     return render_template('8-cities_by_states.html', states=states)
 
 
